3701f07ecda4351627fd1f173c41be3999c9e1f0
banlangen <humingx@yeah.net928012047@qq.com>
2019-03-14 16:15:51 +0800

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+
+// rollup.config.js
+// import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser'
+export default {    
+  input: './packages/index.js',
+  output: {
+    file: './lib/vue-savedata.umd.js',
+    format: 'umd'
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**', // 只编译我们的源代码
+      runtimeHelpers: true
+    }),
+    terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+    })
+  ]
+};
